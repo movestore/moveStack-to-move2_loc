f666c5e351eeddaa59701ea65111f1a6ac81a214
andreakoelzsch <akoelzsch@ab.mpg.de>
2023-01-31 11:09:22 +0100

    first version

--- RFunction.R
+++ RFunction.R
@@ -1,10 +1,15 @@
 library('move')
+library('move2')
 
 ## The parameter "data" is reserved for the data object passed on from the previous app
 
 ## to display messages to the user in the log file of the App in MoveApps one can use the function from the logger.R file: 
 # logger.fatal(), logger.error(), logger.warn(), logger.info(), logger.debug(), logger.trace()
 
-rFunction = function(data, year) {
-  data[lubridate::year(data@timestamps) == year]
+rFunction = function(data) {
+  result <- mt_as_move2(data)
+  return(result)
 }
+
+#attr(mt_time(result),"tz")
+#attr(timestamps(data),"tz)
